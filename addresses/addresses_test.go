5ed3828e8c0e831093a0eb441e4ffeb9da4def8e
Gustavo Vieira <gustavo.vieira@betalearning.com.br>
2022-05-08 17:42:30 -0300

    add multiple scenarios test

--- addresses/addresses_test.go
+++ addresses/addresses_test.go
@@ -2,6 +2,11 @@ package addresses
 
 import "testing"
 
+type testScenario struct {
+	insertedAddress         string
+	expectedReturnedAddress string
+}
+
 // Unit Tests
 // Signature must begin with Test
 
@@ -16,5 +21,25 @@ func TestAddressType(t *testing.T) {
 		t.Errorf("Type mismatch expected - expected %v got %v",
 			expectedAddressType,
 			received)
+	}
+}
+
+func TestAddressTypes(t *testing.T) {
+	scenarios := []testScenario{
+		{"Street something", "street"},
+		{"Plaza whatever", "plaza"},
+		{"Avenue something", "avenue"},
+		{"Road 123", "road"},
+		{"Errado", "Address type is invalid"},
+	}
+
+	for _, scenario := range scenarios {
+
+		addrType := AddresType(scenario.insertedAddress)
+		if scenario.expectedReturnedAddress != addrType {
+			t.Errorf("Type mismatch expected - expected %v got %v",
+				scenario.expectedReturnedAddress,
+				addrType)
+		}
 	}
 }
